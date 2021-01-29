9de59a0cea77a15fd93c99905bedb007c9c1a470
Fernando <fernando@dano.me>
2021-01-29 18:14:16 +0900

    Initial Commit

---
+++ jandi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  jandi
+//
+//  Created by Fernando on 2021/01/29.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ jandi/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  jandi
+//
+//  Created by Fernando on 2021/01/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
